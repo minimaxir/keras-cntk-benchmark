77721ac28a8cdfc709a2df268bc67a1ca69eccae
Max Woolf <max@minimaxir.com>
2017-06-08 13:19:22 -0700

    Make benchmark script cloud GPU friendly

--- keras_cntk_benchmark.py
+++ keras_cntk_benchmark.py
@@ -6,12 +6,12 @@ if not os.path.exists('logs'):
 test_files = [f for f in os.listdir("test_files") if f.endswith('.py')]
 test_files.remove('CustomCallback.py')
 backends = ['cntk', 'tensorflow']
-docker_cmd = "docker run -it --rm -v $(pwd)/:/keras --name keras"
+docker_cmd = "sudo nvidia-docker run -it --rm -v $(pwd)/:/keras --name keras"
 
 for test_file in test_files:
     for backend in backends:
         statement = docker_cmd + \
-            " -e KERAS_BACKEND='{}' keras-cntk-cpu python3 test_files/{}".format(
+            " -e KERAS_BACKEND='{}' minimaxir/keras-cntk python3 test_files/{}".format(
                 backend, test_file)
 
         print(statement)
