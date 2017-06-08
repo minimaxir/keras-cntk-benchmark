a6a9780e42d7d313b375bdecdc575e4692aaab00
Max Woolf <max@minimaxir.com>
2017-06-07 20:14:41 -0700

    Arg arrays to string

--- keras_cntk_benchmark.py
+++ keras_cntk_benchmark.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 if not os.path.exists('logs'):
     os.makedirs('logs')
@@ -7,14 +6,15 @@ if not os.path.exists('logs'):
 test_files = [f for f in os.listdir("test_files") if f.endswith('.py')]
 test_files.remove('CustomCallback.py')
 backends = ['cntk', 'tensorflow']
-docker_cmd = "docker run -it --rm -v $(pwd)/:/keras --name keras".split(" ")
+docker_cmd = "docker run -it --rm -v $(pwd)/:/keras --name keras"
 
 for test_file in test_files:
     for backend in backends:
-        statement = docker_cmd + ["-e", "KERAS_BACKEND='{}'".format(backend), "keras-cntk-cpu",
-                                  "python3", "test_files/" + test_file]
+        statement = docker_cmd + \
+            " -e KERAS_BACKEND='{}' keras-cntk-cpu python3 test_files/{}".format(
+                backend, test_file)
 
-        print(" ".join(statement))
+        print(statement)
         print("{} + {}".format(test_file, backend))
 
-        os.system(" ".join(statement))
+        os.system(statement)
