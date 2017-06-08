867fb49e07e97ad1dcf55eafde4fde5bca1349bd
Max Woolf <max@minimaxir.com>
2017-06-08 10:50:34 -0700

    Advanced text generator

--- test_files/lstm_text_generation.py
+++ test_files/lstm_text_generation.py
@@ -55,7 +55,7 @@ for i, sentence in enumerate(sentences):
 # build the model: a single LSTM
 print('Build model...')
 model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(LSTM(128, input_shape=(maxlen, len(chars)), implementation=2))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
 
@@ -73,7 +73,7 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 # train the model, output generated text after each iteration
-for iteration in range(1, 1):
+for iteration in [1]:
     print()
     print('-' * 50)
     print('Iteration', iteration)
